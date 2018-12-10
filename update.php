79f4b7d1dcb34ff52cb930a47bf217af47a10310
c4539 <me@c4539.com>
2018-12-10 10:47:46 +0100

    solving "Undefined index: ipaddr in update.php on line 134"

--- update.php
+++ update.php
@@ -131,13 +131,13 @@ while ($DbRecordsStmt->fetch()) {
 	switch ($RecordType) {
 		case "A":
 			// update record only if the new IPv4 address is valid
-			if (filter_var($_REQUEST['ipaddr'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+			if (isset($_REQUEST['ipaddr']) && filter_var($_REQUEST['ipaddr'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
 				$Updates[$RecordId] = $_REQUEST['ipaddr'];
 			}
 			break;
 		case "AAAA":
 			// update record only if the new IPv6 address is valid
-			if (filter_var($_REQUEST['ip6addr'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+			if (isset($_REQUEST['ip6addr']) && filter_var($_REQUEST['ip6addr'], FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
 				$Updates[$RecordId] = $_REQUEST['ip6addr'];
 			} else {
 				// Do not update records that should have a IPv6 address outside the lan prefix
